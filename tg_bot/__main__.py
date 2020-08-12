5340c9864ed953513f039fd4f3fc6815dbd94cee
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-12 07:49:14 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,9 +19,9 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 
-Hello {}, Iam a bot and my name is {}😎 Im a group manager bot maintained by  [RoHiTH reDDY](tg://user?id={}).
-No use in messaging me here am available on @BTECHTHOPULU
-**U r not able to do any thing in this so be silent.**
+Hello {}, Iam a bot and my name is {} Im a group manager bot maintained by  [Anonymous](tg://user?id={}).
+No use in messaging me here am available on [anonymous discussion group](https://t.me/joinchat/M_gNMlIDiBJBCfH_hRL7hg)
+**yoU are not able to do any thing in this so be silent.**
 
 """
 
